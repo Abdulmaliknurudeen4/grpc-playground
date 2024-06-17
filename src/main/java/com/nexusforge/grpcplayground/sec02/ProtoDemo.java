f612de792f0ea2df8e9cbd378869eaf3aab45110
Nurudeen Abdulmalik <Abdulmaliknurudeen4@gmail.com>
2024-06-17 15:37:11 +0100

    01 - basic proto file Creating multiple instances, testing the ability of a proto generated class.

--- src/main/java/com/nexusforge/grpcplayground/sec02/ProtoDemo.java
+++ src/main/java/com/nexusforge/grpcplayground/sec02/ProtoDemo.java
@@ -8,12 +8,39 @@ public class ProtoDemo {
     private static final Logger log = LoggerFactory.getLogger(ProtoDemo.class);
 
     public static void main(String[] args) {
-        Person sam = Person.newBuilder()
+        //create person1
+        var person1 = createPerson();
+
+        //create person 2
+        var person2 = createPerson();
+
+        //compare
+        log.info("equals {}", person1.equals(person2));
+        log.info("== {}", person1==(person2));
+
+        //mutable ?? No
+
+        //create another instance with diff values
+        var person3 = person1.toBuilder()
+                .setName("mike").build();
+
+        //compare
+        log.info("equals {}", person1.equals(person3));
+        log.info("== {}", person1==(person3));
+
+        //null values :: can't take null
+        var person4 = person1.toBuilder()
+                //.setName(null)
+                .clearName()
+                .build();
+        log.info("person4:{}", person4);
+
+    }
+
+    private static Person createPerson() {
+        return Person.newBuilder()
                 .setName("sam")
                 .setAge(45)
                 .build();
-        log.info("{}", sam);
-
-
     }
 }
