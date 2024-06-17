2b64d28e94e717b6407613d956ccfcd9e34b2ca2
Nurudeen Abdulmalik <Abdulmaliknurudeen4@gmail.com>
2024-06-17 13:52:18 +0100

    01 - basic proto file

---
+++ src/main/java/com/nexusforge/grpcplayground/GrpcPlaygroundApplication.java
@@ -0,0 +1,9 @@
+package com.nexusforge.grpcplayground;
+
+public class GrpcPlaygroundApplication {
+
+    public static void main(String[] args) {
+//mark directory as generated source root
+    }
+
+}
---
+++ src/main/java/com/nexusforge/grpcplayground/sec01/SimpleProtoDemo.java
@@ -0,0 +1,19 @@
+package com.nexusforge.grpcplayground.sec01;
+
+import com.nexusforge.grpcplayground.models.PersonOuterClass;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class SimpleProtoDemo {
+    private static final Logger log = LoggerFactory.getLogger(SimpleProtoDemo.class);
+
+    public static void main(String[] args) {
+
+        var sam = PersonOuterClass.Person.newBuilder()
+                .setName("sam")
+                .setAge(12)
+                .build();
+        log.info("{}", sam);
+
+    }
+}
