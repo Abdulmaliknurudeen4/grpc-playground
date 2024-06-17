1e1a2120400cb714eb62ef2bbd4d76155208cddd
Nurudeen Abdulmalik <Abdulmaliknurudeen4@gmail.com>
2024-06-17 23:05:37 +0100

    01 - basic proto Making Maps in proto

---
+++ src/main/java/com/nexusforge/grpcplayground/sec03/Lec06Map.java
@@ -0,0 +1,34 @@
+package com.nexusforge.grpcplayground.sec03;
+
+import com.nexusforge.grpcplayground.models.sec03.Car;
+import com.nexusforge.grpcplayground.models.sec03.Dealer;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.util.Map;
+
+public class Lec06Map {
+    public static final Logger log = LoggerFactory.getLogger(Lec04.class);
+
+    public static void main(String[] args) {
+        var car1 = Car.newBuilder()
+                .setMake("Honda")
+                .setModel("civic")
+                .setYear(2000)
+                .build();
+
+        var car2 = Car.newBuilder()
+                .setMake("Honda")
+                .setModel("accord")
+                .setYear(2030)
+                .build();
+
+        var dealer = Dealer.newBuilder()
+                .putAllInventory(Map.of(car1.getYear(), car1,
+                        car2.getYear(), car2))
+                .build();
+
+        log.info("{}", dealer);
+
+    }
+}
