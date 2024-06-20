85536d2efcbb288ab1ae30b12df0c9f68c47bf75
Nurudeen Abdulmalik <Abdulmaliknurudeen4@gmail.com>
2024-06-21 00:35:13 +0100

    02 - Unary Communication Protocol - developing bank service application.
    Grpc Client Stub Implementation to call services.

---
+++ src/main/java/com/nexusforge/grpcplayground/sec06/GrpcClient.java
@@ -0,0 +1,27 @@
+package com.nexusforge.grpcplayground.sec06;
+
+import com.nexusforge.grpcplayground.models.sec06.BalanceCheckRequest;
+import com.nexusforge.grpcplayground.models.sec06.BankServiceGrpc;
+import io.grpc.ManagedChannelBuilder;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class GrpcClient {
+    public static final Logger log = LoggerFactory.getLogger(GrpcClient.class);
+
+    public static void main(String[] args) {
+
+        var channel = ManagedChannelBuilder
+                .forAddress("localhost", 6565)
+                .usePlaintext()
+                .build();
+        var stub = BankServiceGrpc.newBlockingStub(channel);
+        var balance = stub.getAccountBalance(BalanceCheckRequest
+                .newBuilder()
+                        .setAccountNumber(2)
+                .build());
+
+        log.info("balance : {} ", balance);
+    }
+
+}
