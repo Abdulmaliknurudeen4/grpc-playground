0c127a6599c94793ac22638f582a644cf3e14cb6
Nurudeen Abdulmalik <Abdulmaliknurudeen4@gmail.com>
2024-06-21 00:52:52 +0100

    02 - Unary Communication Protocol - developing bank service application.
    Grpc Client Stub Implementation to call services. Implementing Async STUBs

--- src/main/java/com/nexusforge/grpcplayground/sec06/GrpcClient.java
+++ src/main/java/com/nexusforge/grpcplayground/sec06/GrpcClient.java
@@ -1,27 +1,47 @@
 package com.nexusforge.grpcplayground.sec06;
 
+import com.nexusforge.grpcplayground.models.sec06.AccountBalance;
 import com.nexusforge.grpcplayground.models.sec06.BalanceCheckRequest;
 import com.nexusforge.grpcplayground.models.sec06.BankServiceGrpc;
 import io.grpc.ManagedChannelBuilder;
+import io.grpc.stub.StreamObserver;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+
+import java.time.Duration;
 
 public class GrpcClient {
     public static final Logger log = LoggerFactory.getLogger(GrpcClient.class);
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws InterruptedException {
 
         var channel = ManagedChannelBuilder
                 .forAddress("localhost", 6565)
                 .usePlaintext()
                 .build();
-        var stub = BankServiceGrpc.newBlockingStub(channel);
-        var balance = stub.getAccountBalance(BalanceCheckRequest
+        var stub = BankServiceGrpc.newStub(channel);
+        stub.getAccountBalance(BalanceCheckRequest
                 .newBuilder()
-                        .setAccountNumber(2)
-                .build());
+                .setAccountNumber(2)
+                .build(), new StreamObserver<AccountBalance>() {
+            @Override
+            public void onNext(AccountBalance accountBalance) {
+                log.info("{}", accountBalance);
+            }
 
-        log.info("balance : {} ", balance);
+            @Override
+            public void onError(Throwable throwable) {
+
+            }
+
+            @Override
+            public void onCompleted() {
+                log.info("completed.");
+            }
+        });
+
+        log.info("Done");
+        Thread.sleep(Duration.ofSeconds(5));
     }
 
 }
