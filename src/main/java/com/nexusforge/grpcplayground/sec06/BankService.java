ffd44f6ef85bf3ca51a91e66aa23f3bfe2334775
Nurudeen Abdulmalik <Abdulmaliknurudeen4@gmail.com>
2024-06-20 18:35:20 +0100

    02 - Unary Communication Protocol - developing bank service application.
    Develop repository

--- src/main/java/com/nexusforge/grpcplayground/sec06/BankService.java
+++ src/main/java/com/nexusforge/grpcplayground/sec06/BankService.java
@@ -3,15 +3,17 @@ package com.nexusforge.grpcplayground.sec06;
 import com.nexusforge.grpcplayground.models.sec06.AccountBalance;
 import com.nexusforge.grpcplayground.models.sec06.BalanceCheckRequest;
 import com.nexusforge.grpcplayground.models.sec06.BankServiceGrpc;
+import com.nexusforge.grpcplayground.sec06.repository.AccountRepository;
 import io.grpc.stub.StreamObserver;
 
 public class BankService extends BankServiceGrpc.BankServiceImplBase {
     @Override
     public void getAccountBalance(BalanceCheckRequest request, StreamObserver<AccountBalance> responseObserver) {
         var accountNumber = request.getAccountNumber();
+        var balance = AccountRepository.getBalance(accountNumber);
         var accountBalance = AccountBalance.newBuilder().
                 setAccountNumber(accountNumber)
-                .setBalance(accountNumber * 10)
+                .setBalance(balance)
                 .build();
         responseObserver.onNext(accountBalance);
         responseObserver.onCompleted();
