f6f0e2240c08d83266d7c71ef60135dc09ee75d1
Nurudeen Abdulmalik <Abdulmaliknurudeen4@gmail.com>
2024-06-21 01:22:45 +0100

    02 - Unary Communication Protocol - developing bank service application.
    GetAllAccounts implemented in User Code

--- src/main/java/com/nexusforge/grpcplayground/sec06/BankService.java
+++ src/main/java/com/nexusforge/grpcplayground/sec06/BankService.java
@@ -1,6 +1,8 @@
 package com.nexusforge.grpcplayground.sec06;
 
+import com.google.protobuf.Empty;
 import com.nexusforge.grpcplayground.models.sec06.AccountBalance;
+import com.nexusforge.grpcplayground.models.sec06.AllAccountResponse;
 import com.nexusforge.grpcplayground.models.sec06.BalanceCheckRequest;
 import com.nexusforge.grpcplayground.models.sec06.BankServiceGrpc;
 import com.nexusforge.grpcplayground.sec06.repository.AccountRepository;
@@ -16,6 +18,22 @@ public class BankService extends BankServiceGrpc.BankServiceImplBase {
                 .setBalance(balance)
                 .build();
         responseObserver.onNext(accountBalance);
+        responseObserver.onCompleted();
+    }
+
+    @Override
+    public void getAllAccounts(Empty request, StreamObserver<AllAccountResponse> responseObserver) {
+        var accounts = AccountRepository.getAllAccounts()
+                .entrySet()
+                .stream().map(e -> AccountBalance.newBuilder()
+                        .setAccountNumber(e.getKey())
+                        .setBalance(e.getValue())
+                        .build())
+                .toList();
+        var response = AllAccountResponse.newBuilder()
+                .addAllAccounts(accounts)
+                .build();
+        responseObserver.onNext(response);
         responseObserver.onCompleted();
     }
 }
