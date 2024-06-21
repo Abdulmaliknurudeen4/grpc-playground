db580c28d4ee0cab6bf3abd6032e65c12a753b36
Nurudeen Abdulmalik <Abdulmaliknurudeen4@gmail.com>
2024-06-21 02:01:15 +0100

    02 - Unary Communication Protocol - developing bank service application.
    Implementing ResponseObserver to handle async calls

--- src/test/java/com/nexusforge/grpcplayground/sec06/Lec02UnaryAsynClientTest.java
+++ src/test/java/com/nexusforge/grpcplayground/sec06/Lec02UnaryAsynClientTest.java
@@ -1,10 +1,10 @@
 package com.nexusforge.grpcplayground.sec06;
 
 import com.google.protobuf.Empty;
+import com.nexusforge.grpcplayground.common.ResponseObserver;
 import com.nexusforge.grpcplayground.models.sec06.AccountBalance;
 import com.nexusforge.grpcplayground.models.sec06.AllAccountResponse;
 import com.nexusforge.grpcplayground.models.sec06.BalanceCheckRequest;
-import io.grpc.stub.StreamObserver;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.slf4j.Logger;
@@ -20,52 +20,24 @@ public class Lec02UnaryAsynClientTest extends AbstractTest {
         var request = BalanceCheckRequest.newBuilder()
                 .setAccountNumber(1)
                 .build();
-        var latch = new CountDownLatch(1);
-        this.stub.getAccountBalance(request, new StreamObserver<AccountBalance>() {
-            @Override
-            public void onNext(AccountBalance accountBalance) {
-                log.info("Async balance received: {}", accountBalance);
-                Assertions.assertEquals(100, accountBalance.getBalance());
-                latch.countDown();
-            }
+        var observer = ResponseObserver.<AccountBalance>create();
+        this.stub.getAccountBalance(request, observer);
+        observer.await();
+        Assertions.assertEquals(1, observer.getItems().size());
+        Assertions.assertEquals(100, observer.getItems().getFirst().getBalance());
+        Assertions.assertNull(observer.getThrowable());
 
-            @Override
-            public void onError(Throwable throwable) {
-                latch.countDown();
-            }
-
-            @Override
-            public void onCompleted() {
-
-            }
-        });
-        latch.await();
     }
 
     @Test
     public void getAllAccounts() throws InterruptedException {
         var request = AllAccountResponse.newBuilder()
                 .build();
-        var latch = new CountDownLatch(1);
-       this.stub.getAllAccounts(Empty.newBuilder().build(), new StreamObserver<AllAccountResponse>() {
-           @Override
-           public void onNext(AllAccountResponse allAccountResponse) {
-               log.info("unary All Accounts received: {} ", allAccountResponse != null);
-               Assertions.assertEquals(10, allAccountResponse.getAccountsList().size());
-               latch.countDown();
-           }
-
-           @Override
-           public void onError(Throwable throwable) {
-               latch.countDown();
-           }
-
-           @Override
-           public void onCompleted() {
-
-           }
-       });
-       latch.await();
-
+        var observer = ResponseObserver.<AllAccountResponse>create();
+        this.stub.getAllAccounts(Empty.newBuilder().build(), observer);
+        observer.await();
+        Assertions.assertEquals(1, observer.getItems().size());
+        Assertions.assertEquals(10, observer.getItems().getFirst().getAccountsCount());
+        Assertions.assertNull(observer.getThrowable());
     }
 }
