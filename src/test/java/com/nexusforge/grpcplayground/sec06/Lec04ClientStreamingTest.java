34c74177485b0775ff336a735a9cc7d962795f1b
Nurudeen Abdulmalik <Abdulmaliknurudeen4@gmail.com>
2024-06-22 00:37:55 +0100

    03 - Server Streaming Communication Protocol.
    Implementation of Client Stream. Integration Tests

---
+++ src/test/java/com/nexusforge/grpcplayground/sec06/Lec04ClientStreamingTest.java
@@ -0,0 +1,42 @@
+package com.nexusforge.grpcplayground.sec06;
+
+import com.nexusforge.grpcplayground.common.ResponseObserver;
+import com.nexusforge.grpcplayground.models.sec06.AccountBalance;
+import com.nexusforge.grpcplayground.models.sec06.DepositRequest;
+import com.nexusforge.grpcplayground.models.sec06.Money;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.util.stream.IntStream;
+
+public class Lec04ClientStreamingTest extends AbstractTest {
+    public static final Logger log = LoggerFactory.getLogger(Lec04ClientStreamingTest.class);
+
+    @Test
+    public void depositTest() {
+        //this.blockingStub -> blocking stub doesn't support client stream
+        var responseObserver = ResponseObserver.<AccountBalance>create();
+        var requestObserver = this.stub.deposit(responseObserver);
+
+        //initial message - accountNumber
+        requestObserver.onNext(DepositRequest.newBuilder()
+                .setAccountNumber(5)
+                .build());
+        IntStream.rangeClosed(1, 10)
+                .mapToObj(i -> Money.newBuilder().setAmount(10).build())
+                .map(c -> DepositRequest.newBuilder().setMoney(c).build())
+                .forEach(requestObserver::onNext);
+
+        requestObserver.onCompleted();
+
+        //at this point our response observer should receive a response
+        responseObserver.await();
+
+        Assertions.assertEquals(1, responseObserver.getItems().size());
+        Assertions.assertEquals(200, responseObserver.getItems().getFirst().getBalance());
+        Assertions.assertNull(responseObserver.getThrowable());
+    }
+
+}
