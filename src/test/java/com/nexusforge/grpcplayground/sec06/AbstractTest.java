e71e18d653bdd0e31da1cd27d9358fc9510db53d
Nurudeen Abdulmalik <Abdulmaliknurudeen4@gmail.com>
2024-06-21 01:07:33 +0100

    02 - Unary Communication Protocol - developing bank service application.
    Preparing TestClasses for Integration Testing

---
+++ src/test/java/com/nexusforge/grpcplayground/sec06/AbstractTest.java
@@ -0,0 +1,24 @@
+package com.nexusforge.grpcplayground.sec06;
+
+import com.nexusforge.grpcplayground.common.AbstractChannelTest;
+import com.nexusforge.grpcplayground.common.GrpcServer;
+import com.nexusforge.grpcplayground.models.sec06.BankServiceGrpc;
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.BeforeAll;
+
+public class AbstractTest extends AbstractChannelTest {
+
+    private final GrpcServer grpcServer = GrpcServer.create(new BankService());
+    protected BankServiceGrpc.BankServiceBlockingStub blockingStub;
+
+    @BeforeAll
+    public void setup() {
+        this.grpcServer.start();
+        this.blockingStub = BankServiceGrpc.newBlockingStub(channel);
+    }
+
+    @AfterAll
+    public void stop() {
+        this.grpcServer.stop();
+    }
+}
