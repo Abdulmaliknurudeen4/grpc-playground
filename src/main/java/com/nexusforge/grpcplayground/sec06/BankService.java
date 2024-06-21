ee8e935c819a0fabc2edf382dc6ecbb2418188b0
Nurudeen Abdulmalik <Abdulmaliknurudeen4@gmail.com>
2024-06-21 23:25:02 +0100

    03 - Server Streaming Communication Protocol.
    Implementation of Streaming response

--- src/main/java/com/nexusforge/grpcplayground/sec06/BankService.java
+++ src/main/java/com/nexusforge/grpcplayground/sec06/BankService.java
@@ -1,11 +1,18 @@
 package com.nexusforge.grpcplayground.sec06;
 
+import com.google.common.util.concurrent.Uninterruptibles;
 import com.google.protobuf.Empty;
 import com.nexusforge.grpcplayground.models.sec06.*;
 import com.nexusforge.grpcplayground.sec06.repository.AccountRepository;
 import io.grpc.stub.StreamObserver;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.util.concurrent.TimeUnit;
 
 public class BankService extends BankServiceGrpc.BankServiceImplBase {
+    private static final Logger log = LoggerFactory.getLogger(BankService.class);
+
     @Override
     public void getAccountBalance(BalanceCheckRequest request, StreamObserver<AccountBalance> responseObserver) {
         var accountNumber = request.getAccountNumber();
@@ -36,6 +43,26 @@ public class BankService extends BankServiceGrpc.BankServiceImplBase {
 
     @Override
     public void withdraw(WithdrawRequest request, StreamObserver<Money> responseObserver) {
+        // do validations, amounts must be in 10 denominations (20 30 40)
+        //account must be valid.
 
+        var accountNumber = request.getAccountNumber();
+        var requestAmount = request.getAmount();
+
+        var accountBalance = AccountRepository.getBalance(accountNumber);
+        if (requestAmount > accountBalance) {
+            responseObserver.onCompleted();
+            return;
+        }
+
+        for (int i = 0; i < requestAmount / 10; i++) {
+            var money = Money.newBuilder().setAmount(10).build();
+            responseObserver.onNext(money);
+            log.info("money sent {} ", money);
+            AccountRepository.deductAmount(accountNumber, 10);
+            Uninterruptibles.sleepUninterruptibly(1, TimeUnit.SECONDS);
+
+        }
+        responseObserver.onCompleted();
     }
 }
