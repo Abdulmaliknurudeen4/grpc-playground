954ba0fe28c37613a2354acead7ce3c1c5f4ffdb
Nurudeen Abdulmalik <Abdulmaliknurudeen4@gmail.com>
2024-07-25 00:49:21 +0100

    Context - Getting Header information into the Service layer 1.Implementing Interceptor to Pass Value to Service layer in the Context Object. 2. Modified and rebuild Server Call with modified Context. 3.Implementing Test Class.

---
+++ src/main/java/com/nexusforge/grpcplayground/sec12/UserRoleBankService.java
@@ -0,0 +1,35 @@
+package com.nexusforge.grpcplayground.sec12;
+
+import com.nexusforge.grpcplayground.models.sec12.*;
+import com.nexusforge.grpcplayground.sec12.repository.AccountRepository;
+import io.grpc.stub.StreamObserver;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class UserRoleBankService extends BankServiceGrpc.BankServiceImplBase {
+    private static final Logger log = LoggerFactory.getLogger(UserRoleBankService.class);
+
+    @Override
+    public void getAccountBalance(BalanceCheckRequest request, StreamObserver<AccountBalance> responseObserver) {
+        var accountNumber = request.getAccountNumber();
+        var balance = AccountRepository.getBalance(accountNumber);
+
+        // this would always return true because it's always true
+        // we're not getting the value from the context.
+        // the context isn't present in this service whatsoever
+        if(UserRole.STANDARD.equals(Constants.USER_ROLE_KEY.get())){
+            var fee = balance > 0 ? 1 :0;
+            AccountRepository.deductAmount(accountNumber, fee);
+            balance = balance - fee;
+        }
+
+        var accountBalance = AccountBalance.newBuilder().
+                setAccountNumber(accountNumber)
+                .setBalance(balance)
+                .build();
+
+        responseObserver.onNext(accountBalance);
+        responseObserver.onCompleted();
+    }
+
+}
---
+++ src/test/java/com/nexusforge/grpcplayground/sec12/Lec07UserRoleContextTest.java
@@ -0,0 +1,87 @@
+package com.nexusforge.grpcplayground.sec12;
+
+import com.nexusforge.grpcplayground.common.GrpcServer;
+import com.nexusforge.grpcplayground.common.ResponseObserver;
+import com.nexusforge.grpcplayground.models.sec12.BalanceCheckRequest;
+import com.nexusforge.grpcplayground.models.sec12.Money;
+import com.nexusforge.grpcplayground.models.sec12.WithdrawRequest;
+import com.nexusforge.grpcplayground.sec12.Interceptors.UserRoleInterceptor;
+import com.nexusforge.grpcplayground.sec12.Interceptors.UserSessionTokenInterceptor;
+import io.grpc.CallCredentials;
+import io.grpc.ClientInterceptor;
+import io.grpc.Metadata;
+import org.junit.jupiter.api.Test;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.util.Collections;
+import java.util.List;
+import java.util.concurrent.Executor;
+import java.util.concurrent.Executors;
+
+public class Lec07UserRoleContextTest extends AbstractInterceptorTest{
+    private static final Logger log = LoggerFactory.getLogger(Lec07UserRoleContextTest.class);
+
+    @Override
+    protected List<ClientInterceptor> getClientInterceptor() {
+        return Collections.emptyList();
+    }
+
+    @Override
+    protected GrpcServer createServer() {
+        return GrpcServer
+                .create(6565, serverBuilder -> {
+                    serverBuilder.addService(new UserRoleBankService())
+                            .intercept(new UserRoleInterceptor());
+                });
+    }
+
+
+    private Metadata getApiKey() {
+        var metadata = new Metadata();
+        metadata.put(Constants.API_KEY, "bank-client-secret");
+        return metadata;
+    }
+
+    @Test
+    public void unaryUserCredentialsDemo() {
+
+        for (int i = 1; i <= 5; i++) {
+
+            var request = BalanceCheckRequest.newBuilder()
+                    .setAccountNumber(i)
+                    .build();
+
+            var response = this.bankBlockingStub
+                    .withCallCredentials(new UserSessionToken("user-token-"+i))
+                    .getAccountBalance(request);
+
+            log.info("{}", response);
+        }
+
+    }
+
+
+    private static class UserSessionToken extends CallCredentials{
+
+        private final String jwt;
+        private static final String TOKEN_FORMAT = "%s %s";
+
+        public UserSessionToken(String jwt) {
+            this.jwt = jwt;
+        }
+
+        @Override
+        public void applyRequestMetadata(RequestInfo requestInfo,
+                                         Executor executor, MetadataApplier metadataApplier) {
+
+            //incase a long running jwt generation shi
+            executor.execute(()->{
+                var metadata = new Metadata();
+                // Autorization Bearer criptyic
+                metadata.put(Constants.USER_TOKEN_KEY, TOKEN_FORMAT.formatted(Constants.BEARER, jwt));
+                metadataApplier.apply(metadata);
+            });
+        }
+    }
+}
