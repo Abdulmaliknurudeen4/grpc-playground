8ae5e39ee30971ccc83fb3360ed8393a746ee8af
Nurudeen Abdulmalik <Abdulmaliknurudeen4@gmail.com>
2024-07-25 00:16:39 +0100

    Interceptor/Metadata 1. Creating UserCredetials Jwt Simulation 2. Extract, check and check for Validity 3. Check Message type(single or Streaming) to validate with users. 4. Writing Test

--- src/test/java/com/nexusforge/grpcplayground/sec12/Lec06UserSessionTokenInterceptorTest.java
+++ src/test/java/com/nexusforge/grpcplayground/sec12/Lec06UserSessionTokenInterceptorTest.java
@@ -1,12 +1,14 @@
 package com.nexusforge.grpcplayground.sec12;
 
 import com.nexusforge.grpcplayground.common.GrpcServer;
+import com.nexusforge.grpcplayground.common.ResponseObserver;
 import com.nexusforge.grpcplayground.models.sec12.BalanceCheckRequest;
-import com.nexusforge.grpcplayground.sec12.Interceptors.ApiKeyValidationInterceptor;
+import com.nexusforge.grpcplayground.models.sec12.Money;
+import com.nexusforge.grpcplayground.models.sec12.WithdrawRequest;
+import com.nexusforge.grpcplayground.sec12.Interceptors.UserSessionTokenInterceptor;
 import io.grpc.CallCredentials;
 import io.grpc.ClientInterceptor;
 import io.grpc.Metadata;
-import io.grpc.stub.MetadataUtils;
 import org.junit.jupiter.api.Test;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -14,6 +16,7 @@ import org.slf4j.LoggerFactory;
 import java.util.Collections;
 import java.util.List;
 import java.util.concurrent.Executor;
+import java.util.concurrent.Executors;
 
 public class Lec06UserSessionTokenInterceptorTest extends AbstractInterceptorTest{
     private static final Logger log = LoggerFactory.getLogger(Lec06UserSessionTokenInterceptorTest.class);
@@ -28,7 +31,7 @@ public class Lec06UserSessionTokenInterceptorTest extends AbstractInterceptorTes
         return GrpcServer
                 .create(6565, serverBuilder -> {
                     serverBuilder.addService(new BankService())
-                            .intercept(new UserSessionTokenInter());
+                            .intercept(new UserSessionTokenInterceptor());
                 });
     }
 
@@ -40,18 +43,39 @@ public class Lec06UserSessionTokenInterceptorTest extends AbstractInterceptorTes
     }
 
     @Test
-    public void userCredentialsDemo() {
+    public void unaryUserCredentialsDemo() {
 
+        for (int i = 1; i <= 5; i++) {
 
-        var request = BalanceCheckRequest.newBuilder()
-                .setAccountNumber(1)
-                .build();
+            var request = BalanceCheckRequest.newBuilder()
+                    .setAccountNumber(1)
+                    .build();
 
-        var response = this.bankBlockingStub
-                .withCallCredentials(new UserSessionToken(""))
-                .getAccountBalance(request);
+            var response = this.bankBlockingStub
+                    .withCallCredentials(new UserSessionToken("user-token-"+i))
+                    .getAccountBalance(request);
 
-        log.info("{}", response);
+            log.info("{}", response);
+        }
+
+    }
+
+    @Test
+    public void streamingUserCredentialsDemo() {
+
+        for (int i = 1; i <= 5; i++) {
+
+            var observer = ResponseObserver.<Money>create();
+            var request = WithdrawRequest.newBuilder()
+                    .setAccountNumber(i)
+                    .setAmount(30)
+                    .build();
+            this.bankStub
+                    .withCallCredentials(new UserSessionToken("user-token-"+i))
+                    .withExecutor(Executors.newVirtualThreadPerTaskExecutor())
+                    .withdraw(request, observer);
+            observer.await();
+        }
 
     }
 
