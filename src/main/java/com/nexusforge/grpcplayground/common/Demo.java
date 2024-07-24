d3d372986473bf5b4d6d9d484c1eefea207124ec
Nurudeen Abdulmalik <Abdulmaliknurudeen4@gmail.com>
2024-07-24 23:27:48 +0100

    Interceptor/Metadata 1. Implementing API Key - server side. Authentication of API 2. Testing with Postman

--- src/main/java/com/nexusforge/grpcplayground/common/Demo.java
+++ src/main/java/com/nexusforge/grpcplayground/common/Demo.java
@@ -1,12 +1,18 @@
 package com.nexusforge.grpcplayground.common;
 
 import com.nexusforge.grpcplayground.sec12.BankService;
+import com.nexusforge.grpcplayground.sec12.Interceptors.ApiKeyValidationInterceptor;
 
 public class Demo {
     public static void main(String[] args) {
-        GrpcServer.create(new BankService())
+      /*  GrpcServer.create(new BankService())
                 .start()
-                .await();
+                .await();*/
+        GrpcServer
+                .create(6565, serverBuilder -> {
+                    serverBuilder.addService(new BankService())
+                            .intercept(new ApiKeyValidationInterceptor());
+                }).start().await();
     }
 
     /*private static class BankInstance1{
