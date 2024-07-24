3a2e20546821bfa96ba28e3cce55823883423450
Nurudeen Abdulmalik <Abdulmaliknurudeen4@gmail.com>
2024-07-24 15:26:43 +0100

    Channel and LoadBalancing 1. Keep Alive Timeout Configurations 2. Observing GO_AWAY

---
+++ src/test/java/com/nexusforge/grpcplayground/sec11/Lec06KeepAliveDemoTest.java
@@ -0,0 +1,43 @@
+package com.nexusforge.grpcplayground.sec11;
+
+import com.google.common.util.concurrent.Uninterruptibles;
+import com.nexusforge.grpcplayground.common.AbstractChannelTest;
+import com.nexusforge.grpcplayground.common.GrpcServer;
+import com.nexusforge.grpcplayground.models.sec11.BalanceCheckRequest;
+import com.nexusforge.grpcplayground.models.sec11.BankServiceGrpc;
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.util.concurrent.TimeUnit;
+
+public class Lec06KeepAliveDemoTest extends AbstractChannelTest {
+    private static final Logger log = LoggerFactory.getLogger(Lec06KeepAliveDemoTest.class);
+
+    private final GrpcServer grpcServer = GrpcServer.create(new DelayedBankService());
+    protected BankServiceGrpc.BankServiceBlockingStub bankBlockingStub;
+
+    @BeforeAll
+    public void setup() {
+       this.grpcServer.start();
+        this.bankBlockingStub = BankServiceGrpc
+                .newBlockingStub(channel);
+    }
+
+    @AfterAll
+    public void stop() {
+        this.grpcServer.stop();
+    }
+
+    @Test
+    public void lazyChannelDemo(){
+        var request = BalanceCheckRequest.newBuilder()
+                .setAccountNumber(1)
+                .build();
+        Uninterruptibles.sleepUninterruptibly(30, TimeUnit.SECONDS);
+        var response = this.bankBlockingStub.getAccountBalance(request);
+        log.info("{}", response);
+    }
+}
