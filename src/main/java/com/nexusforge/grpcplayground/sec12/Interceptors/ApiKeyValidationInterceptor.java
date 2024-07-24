22654e594d88729a0c941aee279a0936806065ce
Nurudeen Abdulmalik <Abdulmaliknurudeen4@gmail.com>
2024-07-24 23:34:21 +0100

    Interceptor/Metadata 1. Getting Method name so you can refine the interceptor restriction so as to apply validation to some part and exclude other parts.

--- src/main/java/com/nexusforge/grpcplayground/sec12/Interceptors/ApiKeyValidationInterceptor.java
+++ src/main/java/com/nexusforge/grpcplayground/sec12/Interceptors/ApiKeyValidationInterceptor.java
@@ -2,14 +2,21 @@ package com.nexusforge.grpcplayground.sec12.Interceptors;
 
 import com.nexusforge.grpcplayground.sec12.Constants;
 import io.grpc.*;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import java.util.Objects;
 
 public class ApiKeyValidationInterceptor implements ServerInterceptor {
 
+    private static final Logger log = LoggerFactory.getLogger(ApiKeyValidationInterceptor.class);
+
     @Override
     public <ReqT, RespT> ServerCall.Listener<ReqT> interceptCall(ServerCall<ReqT, RespT> serverCall,
                                                                  Metadata metadata, ServerCallHandler<ReqT, RespT> serverCallHandler) {
+
+
+        log.info("{}",  serverCall.getMethodDescriptor().getFullMethodName());
 
         var apikey = metadata.get(Constants.API_KEY);
         if (isValid(apikey)) {
