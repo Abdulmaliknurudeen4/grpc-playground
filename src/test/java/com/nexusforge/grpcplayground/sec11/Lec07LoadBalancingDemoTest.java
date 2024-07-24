b353791be71e1fb783a992394ba780d41e470990
Nurudeen Abdulmalik <Abdulmaliknurudeen4@gmail.com>
2024-07-24 16:03:08 +0100

    Channel and LoadBalancing 1. LoadBalancing Using Nginx INfrastructure approach 2. Client Streaming Load Balancing - > Correct Behavior(All streams to a single server)

--- src/test/java/com/nexusforge/grpcplayground/sec11/Lec07LoadBalancingDemoTest.java
+++ src/test/java/com/nexusforge/grpcplayground/sec11/Lec07LoadBalancingDemoTest.java
@@ -1,8 +1,9 @@
 package com.nexusforge.grpcplayground.sec11;
 
+import com.google.common.util.concurrent.Uninterruptibles;
 import com.nexusforge.grpcplayground.common.AbstractChannelTest;
-import com.nexusforge.grpcplayground.models.sec06.BalanceCheckRequest;
-import com.nexusforge.grpcplayground.models.sec06.BankServiceGrpc;
+import com.nexusforge.grpcplayground.common.ResponseObserver;
+import com.nexusforge.grpcplayground.models.sec06.*;
 import io.grpc.ManagedChannel;
 import io.grpc.ManagedChannelBuilder;
 import org.junit.jupiter.api.AfterAll;
@@ -11,9 +12,13 @@ import org.junit.jupiter.api.Test;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import java.util.concurrent.TimeUnit;
+import java.util.stream.IntStream;
+
 public class Lec07LoadBalancingDemoTest extends AbstractChannelTest {
     private static final Logger log = LoggerFactory.getLogger(Lec07LoadBalancingDemoTest.class);
     private BankServiceGrpc.BankServiceBlockingStub bankBlockingStub;
+    private BankServiceGrpc.BankServiceStub bankStub;
     private ManagedChannel channel;
 
     @BeforeAll
@@ -22,10 +27,11 @@ public class Lec07LoadBalancingDemoTest extends AbstractChannelTest {
                 .usePlaintext()
                 .build();
         this.bankBlockingStub = BankServiceGrpc.newBlockingStub(channel);
+        this.bankStub = BankServiceGrpc.newStub(channel);
     }
 
     @Test
-    public void eagerChannelDemo() {
+    public void loadBalancingDemo() {
 
 
         for (int i = 1; i < 10; i++) {
@@ -36,6 +42,31 @@ public class Lec07LoadBalancingDemoTest extends AbstractChannelTest {
             var response = bankBlockingStub.getAccountBalance(request);
             log.info("{}", response);
         }
+    }
+
+    @Test
+    public void clientStreamingTest(){
+        var responseObserver = ResponseObserver.<AccountBalance>create();
+        var requestObserver = this.bankStub.deposit(responseObserver);
+
+        //initial message - accountNumber
+        requestObserver.onNext(DepositRequest.newBuilder()
+                .setAccountNumber(5)
+                .build());
+
+        IntStream.rangeClosed(1, 30)
+                .mapToObj(i -> Money.newBuilder().setAmount(10).build())
+                .map(c -> DepositRequest.newBuilder().setMoney(c).build())
+                .forEach(d ->{
+                    Uninterruptibles.sleepUninterruptibly(1, TimeUnit.SECONDS);
+                    requestObserver.onNext(d);
+                });
+
+
+        requestObserver.onCompleted();
+
+        //at this point our response observer should receive a response
+        responseObserver.await();
     }
 
     @AfterAll
